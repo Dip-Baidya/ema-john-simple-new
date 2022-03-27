07a89a83089dbcf8a3e43f2522a1ac1bc8e164ab
Dip-Baidya <dipbaidya.cse@gmail.com>
2022-03-27 21:16:08 +0600

    calculation Update

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -3,12 +3,12 @@ import React from 'react';
 const Cart = (props) => {
     const { cart } = props;
     let total = 0;
-    for(const product of cart) {
-        total  = total + product.price;
+    for (const product of cart) {
+        total = total + product.price;
     }
 
-    const shipping = 15;
-    const tax = (total+ shipping) * 10;
+    const shipping = total > 0 ? 15 : 0;
+    const tax = (total + shipping) * 0.10;
     const grandTotal = total + shipping + tax;
 
     return (
