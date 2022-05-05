05a467222ff06266ed690df02073ea7848dbcd9f
Dip-Baidya <dipbaidya.cse@gmail.com>
2022-05-05 16:45:33 +0600

    54-2 Create order placed and clear cart and stored data

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Shop from './Components/Shop/Shop';
 import OderReview from './Components/OrderReview/OderReview';
 import Inventory from './Components/Inventory/Inventory';
 import NotFound from './Components/NotFound/NotFound';
+import PlaceOrder from './Components/PlaceOrder/PlaceOrder';
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
           <Route path="/shop" element={<Shop />}></Route>
           <Route path="/review" element={<OderReview />}></Route>
           <Route path="/inventory" element={<Inventory />}></Route>
+          <Route path="/placeorder" element={<PlaceOrder />}></Route>
           <Route path="*" element={<NotFound />}></Route>
         </Routes>
       </BrowserRouter>
--- src/Components/OrderReview/OderReview.js
+++ src/Components/OrderReview/OderReview.js
@@ -3,20 +3,30 @@ import useProducts from '../../Hooks/useProducts';
 import useCart from '../../Hooks/useCart';
 import Cart from '../Cart/Cart';
 import ReviewItem from '../ReviewItem/ReviewItem';
-import { removeFromDb } from '../../utilities/fakedb';
+import { clearTheCart, removeFromDb } from '../../utilities/fakedb';
+import { useNavigate } from 'react-router-dom';
 
 const OderReview = () => {
     const [products, setProducts] = useProducts();
     const [cart, setCart] = useCart(products);
+
+    const navigate = useNavigate();
+
     const handleRemove = id => {
         const newCart = cart.filter(product => product.id !== id);
         setCart(newCart);
         removeFromDb(id);
     }
+
+    const handlePlaceOrder = () => {
+        navigate("/placeorder");
+        setCart([]);
+        clearTheCart();
+    }
     return (
         <div className="shop-container">
             <div className="product-container">
-                { 
+                {
                     cart.map(product => <ReviewItem
                         id={product.id}
                         product={product}
@@ -26,7 +36,7 @@ const OderReview = () => {
             </div>
             <div className="cart-container">
                 <Cart cart={cart}>
-                    <button className="btn-regular">Place Order</button>
+                    <button onClick={handlePlaceOrder} className="btn-regular">Place Order</button>
                 </Cart>
             </div>
         </div>
